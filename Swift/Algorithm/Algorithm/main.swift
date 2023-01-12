4812777486512b91daa8ab54b384c857e8af1582
안소정 <80646857+ahneve@users.noreply.github.com>
2023-01-12 18:04:53 +0900

    .

--- Swift/Algorithm/Algorithm/main.swift
+++ Swift/Algorithm/Algorithm/main.swift
@@ -7,5 +7,36 @@
 
 import Foundation
 
-print("Hello, World!")
+var input = Int(readLine()!)!
+var result : Int = 0
+
+solution(input)
+
+func solution(_ input:Int) -> Int {
+    var remain = 1000-input
+    
+    if(remain >= 500 ) {
+        result += remain/500
+        remain = remain - (remain/500)*500
+    }
+    if(remain >= 100 ) {
+        result += remain/100
+        remain = remain - (remain/100)*100
+    }
+    if(remain >= 50 ) {
+        result += remain/50
+        remain = remain - (remain/50)*50
+    }
+    if(remain >= 10 ) {
+        result += remain/10
+        remain = remain - (remain/10)*10
+    }
+    if(remain >= 5 ) {
+        result += remain/5
+        remain = remain - (remain/5)*5
+    }
+    
+    print(result+remain)
+    return result+remain
+}
 
---
+++ Swift/Algorithm/Algorithm/template.swift
@@ -0,0 +1,8 @@
+//
+//  template.swift
+//  Algorithm
+//
+//  Created by 소정의 Mac on 2023/01/12.
+//
+
+import Foundation
