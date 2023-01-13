78bd7a6a529b157e03230665ae80f659c9a80133
안소정 <80646857+ahneve@users.noreply.github.com>
2023-01-13 22:50:33 +0900

    Update main.swift

--- Swift/Algorithm/Algorithm/main.swift
+++ Swift/Algorithm/Algorithm/main.swift
@@ -1,42 +1,14 @@
-//
-//  main.swift
-//  Algorithm
-//
-//  Created by 소정의 Mac on 2023/01/08.
-//
-
 import Foundation
 
-var input = Int(readLine()!)!
+var input = 1000-Int(readLine()!)!
+var JPY : [Int] = [500,100,50,10,5,1]
 var result : Int = 0
 
-solution(input)
-
-func solution(_ input:Int) -> Int {
-    var remain = 1000-input
-    
-    if(remain >= 500 ) {
-        result += remain/500
-        remain = remain - (remain/500)*500
+for i in JPY {
+    if (input >= i){
+        result = result+(input/i)
+        input = input-(input/i)*i
     }
-    if(remain >= 100 ) {
-        result += remain/100
-        remain = remain - (remain/100)*100
-    }
-    if(remain >= 50 ) {
-        result += remain/50
-        remain = remain - (remain/50)*50
-    }
-    if(remain >= 10 ) {
-        result += remain/10
-        remain = remain - (remain/10)*10
-    }
-    if(remain >= 5 ) {
-        result += remain/5
-        remain = remain - (remain/5)*5
-    }
-    
-    print(result+remain)
-    return result+remain
 }
 
+print(result)
